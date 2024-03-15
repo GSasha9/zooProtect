48807a441cfdc5a0e9df0a521e86721f70849bea
Sasha <sasha.hurbnva@gmail.com>
2024-03-16 01:04:33 +0300

    preloader added, improved menu for mobile

--- script/script.js
+++ script/script.js
@@ -1,13 +1,20 @@
-//запускаем видео при загрузке страницы
+//предзагрузчик, запускаем видео, аудио при загрузке страницы
+
 const video = document.querySelector("video");
 const audio = document.querySelectorAll("audio");
 
-window.onload = function (){
-    console.log(video);
-    video.play();
-    audio.play();
-    return;
-}
+document.body.onload = function(){
+    setTimeout(function(){
+    let areaForLoader = document.getElementById('areaForLoader');
+    if (areaForLoader.classList !== "hide")
+    {
+        areaForLoader.classList.add('hide');
+        video.play();
+        audio.play();
+    }
+    }, 3000);
+  }
+
 
 //функция для остановки/вопроизведения видео
 
@@ -54,7 +61,6 @@ function audioStopPlay(){
 
 
 // замена логотипа img на логотип gif
-
 const logoArea = document.querySelector("#header__menu>a");
 const img = document.querySelector("#logo_stat");
 logoArea.addEventListener('mouseover', animateLogo);
@@ -73,7 +79,6 @@ function animateLogo(){
 
 
 //функция для вызова формы регистрации
-
 const buttonEnter = document.querySelectorAll(".enterButton");
 let arrOfButtonEnter = [...buttonEnter];
 arrOfButtonEnter.forEach((elem)=>elem.addEventListener('click', openForm));
@@ -85,7 +90,7 @@ function openForm(){
 
 
 
-//появление метки геотега и инфокарты?
+//появление метки геотега и инфокарты при клике
 let points = document.querySelectorAll('.geoteg');
 let allPoints = [...points];
 const map = document.querySelector('#contacts__map');
@@ -97,7 +102,7 @@ allPoints.forEach(function(el){
 
 const arrOfPicForCard = ['0','./img/volontee/1vitebskDobrik.png','./img/volontee/2vitebskIrina.png','./img/volontee/3vitebskVetCentr.png','./img/volontee/4minskzooch.png','./img/volontee/5mogilevman.png','./img/volontee/6minskKsenya.png','./img/volontee/7mogilevcentr.png','./img/volontee/8grodnoSeredce.png','./img/volontee/9mogilevOlga.png','./img/volontee/10bresrDobr.png','./img/volontee/11gomel.png'];
 const arrOfNamesForCard = ['0','Приют "Добрик"','Ирина. Волонтер','Ветеринарный центр доктора Базылевского А. А','Зоошанс','Дмитрий. Волонтер','Ксения. Волонтер','Ветзооцентр','Преданное сердце','Ольга. Волонтер','Приют "Доброта"','"Подари им шанс"'];
-const arrOfDescripForCard = ['0','г. Витебск, Старобабинический тракт 29. <span>тел. +375(33)336-78-77</span>','Помощь в финансирования медицинских расходов на лечение животных в Витебске.<span>тел. +375(29)345-67-09</span>','г. Витебск, ул. Чкалова, 68.<span>тел. +375(29)344-56-78</span>','г. Минск, ул. Якуба Коласа 50/1. <span>тел. +375(29)191-10-92</span>','помощь в поиске волонтеров для временной передержки животных в Могилеве.<span>тел. +375(29)191-10-92</span>','помощь с кормом для животных в Минске.<span>тел. +375(29)345-65-43</span> ','г.Могилев, ул. Королёва 8<span>тел. +375(29)191-10-92</span>','predannoeserdce.by','Организация медицинской помощи животным в Могилеве.<span>тел. +375(44)453-78-77</span> ','г. Брест, Ковельская улица 1.<span>тел. +375(16)295-81-30</span>','тел. +375(44)673-00-30'];
+const arrOfDescripForCard = ['0','г. Витебск, Старобабинический тракт 29. <span>тел. +375(33)336-78-77</span>','Помощь в финансирования медицинских расходов на лечение животных в Витебске.<span>тел. +375(29)345-67-09</span>','г. Витебск, ул. Чкалова, 68.<span>тел. +375(29)344-56-78</span>','г. Минск, ул. Якуба Коласа 50/1. <span>тел. +375(29)191-10-92</span>','помощь в поиске волонтеров для временной передержки животных в Могилеве.<span>тел. +375(29)191-10-92</span>','помощь с кормом для животных в Минске.<span>тел. +375(29)345-65-43</span> ','г.Могилев, ул. Королёва 8<span>тел. +375(29)191-10-92</span>','predannoeserdce.by, г. Гродно','Организация медицинской помощи животным в Могилеве.<span>тел. +375(44)453-78-77</span> ','г. Брест, Ковельская улица 1.<span>тел. +375(16)295-81-30</span>','г. Брест, тел. +375(44)673-00-30'];
 
 function showInfoCard(el){
 //убираем все значки геотега, если они есть
@@ -115,32 +120,32 @@ function showInfoCard(el){
     geoTeg.style.transform = "translateX(-1vw) translateY(-2.5vw)"; // задаем атрибут width
     diVector.appendChild(geoTeg);
     
-//появление информационной карты
-let contactsInfoCard = document.querySelector('#contacts__card');
-contactsInfoCard.style.opacity = '1';
-let solidLine = document.querySelector('#solid_line');
-solidLine.style.opacity = '1';
-contactsInfoCard.addEventListener('mouseout', function(){geoTeg.style.opacity = '1'});
-//выбор из массивов нужной информации для карточки
-let arrOfLetters = pointId.split('');
-let indexNumber = 0;
+    //появление информационной карты
+    let contactsInfoCard = document.querySelector('#contacts__card');
+    contactsInfoCard.style.opacity = '1';
+    let solidLine = document.querySelector('#solid_line');
+    solidLine.style.opacity = '1';
+    contactsInfoCard.addEventListener('mouseout', function(){geoTeg.style.opacity = '1'});
+    //выбор из массивов нужной информации для карточки
+    let arrOfLetters = pointId.split('');
+    let indexNumber = 0;
 
-if(arrOfLetters.length == 6){
-    indexNumber = arrOfLetters.pop();
-}
-else{
-    indexNumber = arrOfLetters.splice(5,6).join('');
-    console.log(indexNumber);
-}
+    if(arrOfLetters.length == 6){
+        indexNumber = arrOfLetters.pop();
+    }
+    else{
+        indexNumber = arrOfLetters.splice(5,6).join('');
+        console.log(indexNumber);
+    }
 
-let cardsImg = document.querySelector('#contacts__card_info>div>img');
-cardsImg.setAttribute('src', arrOfPicForCard[indexNumber]);
+    let cardsImg = document.querySelector('#contacts__card_info>div>img');
+    cardsImg.setAttribute('src', arrOfPicForCard[indexNumber]);
 
-let cardsName = document.querySelector('#contacts__card_info_title');
-cardsName.textContent = arrOfNamesForCard[indexNumber];
+    let cardsName = document.querySelector('#contacts__card_info_title');
+    cardsName.textContent = arrOfNamesForCard[indexNumber];
 
-let cardsDescr = document.querySelector('#contacts__card_info_descr');
-cardsDescr.innerHTML = arrOfDescripForCard[indexNumber];
+    let cardsDescr = document.querySelector('#contacts__card_info_descr');
+    cardsDescr.innerHTML = arrOfDescripForCard[indexNumber];
 }
 
 //функция проверки есть ли метка геотега на карте и меняет ее на точку
@@ -180,45 +185,40 @@ let arrOfNews = [
     title: 'Домики для животных',
     width: '100%',
     alt: 'Домики для животных'
-    },
+},
     {
     img: './img/newsimg2.png',
     title: 'Наши волонтеры',
     width: '100%',
     alt: 'Волонтеры'
-    },
+},
     {
     img: './img/newsim3.png',
     title: 'История Кузи',
     width: '100%',
     alt: 'Рыжий кот'
-    },
+},
     {
     img: './img/newsimg4.png',
     title: 'Вет клиники',
     width: '100%',
     alt: 'Кот на руках у доктора'
-    },
+},
     {
     img: './img/newsimg5.png',
     title: 'Ваши питомцы',
     width: '100%',
     alt: 'Кот и собакаы'
-    }
+}
 ];
 
 let nIndex = 0;
 let slider = document.querySelector('#allNews__news_slider');
 
 function initSlider(){
-    
-    
         fillGrid(slider, arrOfNews);
-        
     } 
    
-
-
 function fillGrid(slider, arrOfNews){
     slider.innerHTML = '';
     for (let i = 0; i < arrOfNews.length; i++){
@@ -237,31 +237,39 @@ function fillGrid(slider, arrOfNews){
         slider.appendChild(gridItem);
     }
 }
+
 const arrowRight = document.querySelector('#arrow_right');
 arrowRight.addEventListener('click', function(){
     nIndex++;
-   
-    initSlider();
-    console.log(nIndex);  
-})
+    initSlider(); 
+}
+)
+
 const arrowLeft = document.querySelector('#arrow_left');
 arrowLeft.addEventListener('click', function(){
     nIndex--;
     nIndex = (nIndex + arrOfNews.length) % arrOfNews.length;
 initSlider();
-console.log(nIndex);
-})
+}
+)
        
-
 initSlider();
 
 //функция для раскрытия бургер меню в мобильной версии
-document.querySelector('#header__menu_mobile>svg').addEventListener('click', openBurgerMenu);
+document.querySelector('#header__menu_mobile>svg').addEventListener('click', menuBurgerOpen);
 document.querySelector('#burger_menu_head>svg').addEventListener('click', closeBurgerMenu);
 
-function openBurgerMenu(){
-    const burgerMenu = document.querySelector('#burger_menu');
-    burgerMenu.style.left = "0px";
+//следование меню за бургером
+let burgerMenuLabel = document.querySelector('#header__menu_mobile>svg');
+
+function menuBurgerOpen(){
+let hiddenMenu = document.querySelector('#burger_menu');
+let coords = burgerMenuLabel.getBoundingClientRect();
+
+console.log(coords);
+hiddenMenu.style.top = coords.top-70 + scrollY + "px";
+console.log(scrollY);
+hiddenMenu.style.left = coords.left-100 + "px";
 }
 
 function closeBurgerMenu(){
@@ -269,8 +277,15 @@ function closeBurgerMenu(){
     burgerMenu.style.left = "-500px";
 }
 
+//закрытие бургер меню при переходе по ссылке из этого меню
 
+const burgerMenuRef = [...document.querySelectorAll("#burger_menu>ul>li")];
+burgerMenuRef.forEach((elem)=> elem.addEventListener('click',hideBurgerMenuIfLeave ));
 
+function hideBurgerMenuIfLeave(){
+    const burgerMenu = document.querySelector('#burger_menu');
+    burgerMenu.style.left = '-500px';
+}
 
 
 
